75a92bbb91945422bb7d9cb97c42f47855ac89a5
Malahoko MAC :) <malahoko@icloud.com>
2021-04-25 02:59:29 -0400

    Update pivotgrowth sig in footer

--- app/Views/admin/header/footer.php
+++ app/Views/admin/header/footer.php
@@ -1,8 +1,7 @@
 
 
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
+    <strong>Developed By <a style="color:mediumpurple;" href="https://pivotgrowth.io">pivotgrowth.io</a>.</strong>
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.1.0
     </div>
