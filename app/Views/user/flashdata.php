40aad484d14e77adb7d40cdcba305eb2a30c29be
Malahoko MAC :) <malahoko@icloud.com>
2021-05-06 18:27:00 -0400

    FLashdata Styling

--- app/Views/user/flashdata.php
+++ app/Views/user/flashdata.php
@@ -3,7 +3,7 @@ if (isset($message)) : ?>
                             <div class=" alert alert-primary" style=" 
                                color: #004085;
                                 background-color: #cce5ff;
-                                border-color: #b8daff;" role="alert">
+                                border-color: #b8daff;margin-top:20px;" role="alert">
                                 <div class="text-white">
                                  <?php print_r($message); ?>
                                 </div>
@@ -11,7 +11,7 @@ if (isset($message)) : ?>
         <?php endif; ?>
 
         <?php if (isset($successMessage)) : ?>
-                            <div class=" alert alert-success"  role="alert">
+                            <div class=" alert alert-success"  role="alert" style="margin-top:20px;">
                                 <div class="text-white">
                                  <?php print_r($successMessage); ?>
                                 </div>
