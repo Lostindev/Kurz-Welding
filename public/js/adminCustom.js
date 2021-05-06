aacb3d581f0c615983731d68a68d5370cfd3327e
Malahoko MAC :) <malahoko@icloud.com>
2021-05-05 21:59:44 -0400

    Add Riode Template Assets

--- public/js/adminCustom.js
+++ public/js/adminCustom.js
@@ -1,36 +1,24 @@
+
+
 $(function () {
-    $(document).ready(function() {
-        $('#categoryId').change(function(){
-            var category_id = $('#categoryId').val();
-            var action = 'get_sub_cat';
-    
-            if(category_id != '0')
-            {
-                $.ajax({
-                    url:"<?php echo site_url('/admin/action');?>",
-                    method:"POST",
-                    data:{category_id:category_id, action:action},
-                    dataType:"JSON",
-                    success:function(data)
-                    {
-                      console.log(data);
-                        var html = '<option value="0">Select Sub Category</option>';
-    
-                        for(var count = 0; count < data.length; count++)
-                        {
-                            html += '<option value="'+data[count].scId+'">'+data[count].scName+'</option>';
-                        }
-    
-                        $('#subCategory').html(html);
-                    }
-                });
-            }
-            else {
-                $('#subCategory').val('0');
-            }
-    
+    $(document).ready(function () {
+        $('.deleteCat').click(function () {
+            var id = $(this).data('id');
+            var text = $(this).data('id');
+            $.ajax({
+                type : 'POST',
+                url : surl+'admin/deleteCategory',
+                data : {
+                    id:id,
+                    text:text
+                },
+                success:function (data) {
+                    console.log(data);
+                },
+                error:function () {
+
+                }
+            });
         });
-    
     });
-    })
-    
+})
