946029c4ca10876ecfb5e1127ad384d54f29ed39
Malahoko MAC :) <malahoko@icloud.com>
2021-05-11 03:07:49 -0400

    Gallery Links

--- app/Views/gallery/index.php
+++ app/Views/gallery/index.php
@@ -1,9 +1,18 @@
 <main class="main">
+<div class="page-header"
+	style="background-image: url('/theme/images/shop/page-header-back.jpg'); background-color: #3C63A4;">
+	<h1 class="page-title">Art Gallery</h1>
+	<ul class="breadcrumb">
+		<li><a href="/"><i class="d-icon-home"></i></a></li>
+		<li class="delimiter">/</li>
+		<li>Gallery</li>
+	</ul>
+</div>
             <nav class="breadcrumb-nav">
                 <div class="container">
                     <ul class="breadcrumb">
                         <li><a href="/"><i class="d-icon-home"></i></a></li>
-                        <li><a href="#" class="active">Home</a></li>
+                        <li><a href="/" class="active">Home</a></li>
                         <li>Gallery</li>
                     </ul>
                 </div>
