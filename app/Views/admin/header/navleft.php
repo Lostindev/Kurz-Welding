400ef80d40093e64fc843f3d952600f5b1baaa93
Malahoko MAC :) <malahoko@icloud.com>
2021-06-08 12:57:56 -0400

    Admin Navbar

--- app/Views/admin/header/navleft.php
+++ app/Views/admin/header/navleft.php
@@ -2,8 +2,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="/assets/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Admin Panel</span>
+      <img src="https://i.pinimg.com/564x/c6/ca/90/c6ca900eee3cd55f2c43ac29636cf2ef.jpg" alt="PGM Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">PGM Admin Panel</span>
     </a>
 
     <!-- Sidebar -->
@@ -11,7 +11,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="/assets/adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="/img/logo.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Kurz Welding</a>
